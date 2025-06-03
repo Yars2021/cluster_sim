6b64108f3be7765f97bb8e9386c0442372b6a5c8
Yars2021 <79992244+Yars2021@users.noreply.github.com>
2025-06-03 14:54:12 +0300

    Create monitor.erl

---
+++ src/monitor.erl
@@ -0,0 +1,24 @@
+-module(monitor).
+
+-export([init_cluster/2]).
+
+
+init_cluster(ClusterSize, BrokenNodes) ->
+    Cluster = [{ID, spawn(fun() -> worker:init(self(), ID) end)} || ID <- lists:seq(1, ClusterSize)],
+    
+    lists:foreach(fun({_, PidFrom}) ->
+        lists:foreach(fun({_, PidTo}) ->
+            PidFrom ! {connect, PidTo}
+        end, lists:filter(fun({_, Pid}) ->
+            Pid /= PidFrom
+        end, Cluster))
+    end, Cluster),
+
+    lists:foreach(fun({_, Pid}) ->
+        Pid ! {break}
+    end, [lists:nth(rand:uniform(ClusterSize), Cluster) || _ <- lists:seq(1, BrokenNodes)]),
+
+    set_strategy(Cluster, a).
+
+
+set_strategy(, strat) -> todo.
