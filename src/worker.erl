e733193a24d6388365e3d2fc93fd9f496dfc82e5
Yars2021 <79992244+Yars2021@users.noreply.github.com>
2025-06-03 14:53:54 +0300

    Create worker.erl

---
+++ src/worker.erl
@@ -0,0 +1,84 @@
+-module(worker).
+
+-export([init/2]).
+
+
+init(Monitor, ID) ->
+    main_routine(Monitor, ID, ok, not_specified, []).
+
+
+main_routine(Monitor, ID, Status, Strategy, Connections) ->
+    receive
+        {break} ->
+            main_routine(Monitor, ID, broken, Strategy, Connections);
+
+        {fix} ->
+            main_routine(Monitor, ID, ok, Strategy, Connections);
+
+        {strategy, NewStrategy} ->
+            main_routine(Monitor, ID, Status, NewStrategy, Connections);
+
+        {connect, Node} ->
+            main_routine(Monitor, ID, Status, Strategy, lists:sort([Node | Connections]));
+
+        {ping, 0} ->
+            Monitor ! {ping_recv, ID, ttl_end},
+            main_routine(Monitor, ID, Status, Strategy, Connections);
+
+        {ping, TTL} ->
+            Monitor ! {ping_recv, ID, case Status of
+                ok ->
+                    case Strategy of
+                        {single_cast} ->
+                            ping_next(ID + 1, 1, TTL - 1, Connections),
+                            ok;
+
+                        {multicast, GroupSize} ->
+                            ping_next(ID + 1, GroupSize, TTL - 1, Connections),
+                            ok;
+
+                        {broadcast} ->
+                            ping_next(1, length(Connections), TTL - 1, Connections),
+                            ok;
+
+                        {gossip} ->
+                            ping_next(rand:uniform(length(Connections)), 1, TTL - 1, Connections),
+                            ok;
+
+                        _ -> unknown_strategy
+                    end;
+
+                _ -> ping_fail
+            end},
+
+            main_routine(Monitor, ID, Status, Strategy, Connections);
+
+        _ ->
+            main_routine(Monitor, ID, Status, Strategy, Connections)
+    end.
+
+
+ping_next(_, _, 0, _) ->
+    0;
+
+ping_next(StartIndex, Size, TTL, Connections) when Size > length(Connections) ->
+    ping_next(StartIndex, length(Connections), TTL, Connections);
+
+ping_next(StartIndex, Size, TTL, Connections) ->
+    lists:foreach(fun({_, Pid}) -> Pid ! {ping, TTL} end, get_group(StartIndex, Size, Connections)).
+
+
+get_group(_, 0, _) ->
+    [];
+
+get_group(_, _, []) ->
+    [];
+
+get_group(Start, Size, Connections) when Size > length(Connections) ->
+    get_group(Start, length(Connections), Connections);
+
+get_group(Start, Size, [{ID, _} | Tail]) when ID < Start ->
+    get_group(Start, Size, Tail);
+
+get_group(Start, Size, [Head | Tail]) ->
+    [Head | get_group(Start + 1, Size - 1, Tail)].
